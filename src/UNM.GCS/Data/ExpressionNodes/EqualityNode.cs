630a87955e14048d48a5c02242bee9869362d54b
Shawn Cowles <shawn.cowles@gmail.com>
2016-05-17 17:55:27 -0700

    Implementation of UnmParserPostProcessor and VariableAvailabilityExpressionEvaluator.

---
+++ src/UNM.GCS/Data/ExpressionNodes/EqualityNode.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections.Generic;
+using UNM.Parser.SimpleLexer;
+
+namespace UNM.GCS.Data.ExpressionNodes
+{
+    internal class EqualityNode : ExpressionNode
+    {
+        internal EqualityNode(Token token)
+            :base(token)
+        {
+        }
+
+        internal override bool Evaluate(Dictionary<string, string> variables)
+        {
+            if(Right == null)
+            {
+                throw new ExpressionParseException(string.Format(
+                    "Equality operator at {0} without preceding variable.",
+                    Token.Position.Index));
+            }
+
+            if (Left == null)
+            {
+                throw new ExpressionParseException(string.Format(
+                    "Equality operator at {0} without following variable or value.",
+                    Token.Position.Index));
+            }
+
+            var left = "";
+            
+            if(Left is VariableNode)
+            {
+                var leftKey = Left.Token.Value;
+
+                if(variables.ContainsKey(leftKey))
+                {
+                    left = variables[leftKey];
+                }
+                else
+                {
+                    throw new ExpressionParseException(string.Format(
+                        "Expected variable {0}, but it was not found.",
+                        leftKey));
+                }
+
+                if(Right is ValueNode)
+                {
+                    var right = Right.Token.Value;
+
+                    return left == right;
+                }
+                else if( Right is VariableNode)
+                {
+                    var rightKey = Right.Token.Value;
+
+                    if (variables.ContainsKey(rightKey))
+                    {
+                        return left == variables[rightKey];
+                    }
+                    else
+                    {
+                        throw new ExpressionParseException(string.Format(
+                            "Expected variable {0}, but it was not found.",
+                            rightKey));
+                    }
+                }
+                else
+                {
+                    throw new ExpressionParseException(string.Format(
+                        "Variable expected at index {0} but {1} found instead.",
+                        Right.Token.Position.Index,
+                        Right.Token.Type));
+                }
+            }
+            else
+            {
+                throw new ExpressionParseException(string.Format(
+                        "Variable expected at index {0} but {1} found instead.",
+                        Left.Token.Position.Index,
+                        Left.Token.Type));
+            }
+        }
+    }
+}
---
+++ src/UNM.GCS/Implementation/UnmParserPostProcessor.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using UNM.GCS.Interfaces;
+using UNM.Parser;
+
+namespace UNM.GCS.Implementation
+{
+    /// <summary>
+    /// A post processor that runs the response text through a UNM <see cref="NameParser"/>.
+    /// </summary>
+    public class UnmParserPostProcessor : IPostProcessor
+    {
+        private NameParser _nameParser;
+
+        /// <summary>
+        /// Construct a new UnmParserPostProcessor.
+        /// </summary>
+        /// <param name="namelistSource">The namelist source to use for the <see cref="NameParser"/>.</param>
+        public UnmParserPostProcessor(INamelistSource namelistSource)
+        {
+            _nameParser = new NameParser(namelistSource, DateTime.Now.Millisecond);
+
+            _nameParser.Initialize();
+        }
+
+        /// <summary>
+        /// Process response text.
+        /// </summary>
+        /// <param name="response">The text of the response.</param>
+        /// <param name="variables">The variables passed into the <see cref="IConversationSystem"/>.</param>
+        /// <returns>The processed text of the response.</returns>
+        public string Process(string response, Dictionary<string, string> variables)
+        {
+            var parameters = new PatternProcessingParameters(response)
+            {
+                CapitalizationScheme = CapitalizationScheme.BY_SENTENCE,
+                Variables = variables
+            };
+
+            return _nameParser.Process(parameters);
+        }
+    }
+}
